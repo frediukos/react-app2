0b17507c4db3e5a20ad2fc8a05fa164cfb188770
olenafrediuk <frediuk_olena@ukr.net>
2020-05-23 00:15:46 +0300

    Props Fisher + call back

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,10 +3,16 @@ import '../App.css';
 import FooterMenu from "./FooterMenu";
 
 function Footer(props) {
+
+  const change = (e) => {
+    props.attempt(e.target.value)
+  };
+
   return (
     <div>
       <FooterMenu footerMenu={props.footerMenu}/>
       <button onClick={() => props.attempt('Tilapia')}>Tilapia</button>
+      <input type="text" onChange={change}/>
     </div>
   );
 }
